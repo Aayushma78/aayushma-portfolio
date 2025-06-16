9879b6214cdd1978667851a89734fcf9b0f937d6
Aayushma <146149566+Aayushma78@users.noreply.github.com>
2025-06-16 17:21:37 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Hello, I’m Aayushma 👋</h1>
+      <p>Welcome to my portfolio website!</p>
+    </div>
+  );
+}
+
